ec47316ebc527ded12b32db09fb5b25199768fb8
grace <grace.c.youngblood@gmail.com>
2021-02-24 10:37:50 -0500

    Add some markup; `bisque` on `coral`.

--- src/packs/application.js
+++ src/packs/application.js
@@ -8,6 +8,31 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import React from 'react'
+import ReactDOM from 'react-dom'
+import styled from "styled-components"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+const Hello = props => (
+  <Page>Hello {props.name}!</Page>
+)
+
+var Page = styled.div`
+background: bisque;
+height: 80vh;
+width: 60vw;
+margin: 4rem auto;
+padding: 2rem;
+box-shadow: 0 0 24px grey;
+font-family: sans-serif;
+`
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Hello name="you" />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
